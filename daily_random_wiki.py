ad572657961dbd257d3f0184c3e012f38f26ae85
Бесфамильный Владислав <nezerblah@MacBook-Pro-Besfamilnyj.local>
2026-02-12 20:50:14 +0300

    FIRST WORK VERSION

--- daily_random_wiki.py
+++ daily_random_wiki.py
@@ -16,8 +16,8 @@ import requests
 # --------------------- НАСТРОЙКИ ---------------------
 TOKEN = "8234184501:AAEu77D5t2D1FvzxaOpZ4HyyYAaD9qLHmyw"  # токен от BotFather
 ADMIN_CHAT_ID = -1003753027344                           # группа или личка
-SEND_HOUR = 20
-SEND_MINUTE = 47
+SEND_HOUR = 9
+SEND_MINUTE = 30
 
 MOSCOW_TZ = pytz.timezone('Europe/Moscow')
 
